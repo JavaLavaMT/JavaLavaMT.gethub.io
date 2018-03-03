b64ade52d589bffbfd454959f3a7da4fc7d0c322
Jeremy Cherer <chererjeremy@gmail.com>
2018-03-03 14:27:55 -0700

    Create JS.js

---
+++ JS.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  console.log("Okay I'm ready");
+  backDrop();
+});
+
+function backDrop(){
+ var str = "Logic Lab 406 - ";
+    document.getElementById("demo").innerHTML = str.repeat(8000);
+}
